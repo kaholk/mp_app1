274491b91be9f46f4af1a035081df422f77b891d
kaholk <karol.brodka@outlook.com>
2023-03-13 20:50:46 +0100

    init

---
+++ src/backend/index.ts
@@ -0,0 +1,7 @@
+
+
+
+import {startServer} from "./server"
+import {config} from "./config"
+
+startServer(config.server)
---
+++ src/backend/routes/status/get.status.ts
@@ -0,0 +1,8 @@
+
+
+import { RequestHandler } from 'express'
+
+
+export const getStatus: RequestHandler = (req, res) => {
+    res.send("I'm alive")
+}
